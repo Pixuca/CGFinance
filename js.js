b1e80a36ba225989a15a50e37b78d6a955b908c1
Unknown <gvieiradec@gmail.com>
2020-10-21 17:42:20 -0300

    storage

--- js.js
+++ js.js
@@ -56,3 +56,28 @@ function total() {
     precototal.innerHTML = rs ;
 
 }
+function storageAvailable(type){
+    var storage;
+    try {
+        storage = window[type];
+        var x = '_storage_test_';
+        storage.setItem(x,x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e){ 
+        return e instanceof DOMException && (
+            e.code === 22 ||
+            e.code === 1014 ||
+            e.name === 'QuotaExceededError' ||
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            (storage && storage.length !==0);
+        )
+    }
+}
+if (storageAvailable('localStorage')) {
+
+}
+else {
+    
+}
